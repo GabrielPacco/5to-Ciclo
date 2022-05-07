f4031ae008635f529f5132ad0014c55ce3286dab
GabrielPacco <gpaccohu@unsa.edu.pe>
2022-05-07 12:16:39 -0500

    Inicializando directorios

---
+++ Compiladores/Práctica_Nro_02/Diagrama_de_Transicion.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string input = "temp1";
+
+    char simbolo = input[0];
+    int estado = 1;
+    int d = 0;
+
+    while(simbolo != input[size(input)]){
+        switch(estado){
+            case1:
+                if(isdigit(simbolo)) estado = 2;
+                else if(isalpha(simbolo)) estado = 3;
+                else{
+                    cout<< "Error "<< endl;
+                    break;
+                }
+            case2:
+                cout<< "Error "<< endl;
+                break;
+
+            case3:
+                if(isalpha(simbolo)) estado = 3;
+                else if(isdigit(simbolo)) estado = 3;
+                else{
+                    cout<< "Error "<<endl;
+                    break;}
+        }
+        d++;
+        simbolo=input[d];
+        cout<<"Correcto "<< endl;
+    }
+    if(estado!=3)
+    cout<<"Error "<< endl;
+
+    return 0;
+}
